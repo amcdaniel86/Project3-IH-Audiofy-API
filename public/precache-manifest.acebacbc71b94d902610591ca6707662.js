b1babb0121d0c0bdbf207bb2bdefe0dfcf63eebf
Alex McDaniel <amcdaniel186@gmail.com>
2018-12-21 11:00:49 -0500

    moved public, favicon again

---
+++ public/precache-manifest.acebacbc71b94d902610591ca6707662.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cd37200121dd68d54219",
+    "url": "/static/js/main.cd372001.chunk.js"
+  },
+  {
+    "revision": "e0bc2cd71dfcf3c384ea",
+    "url": "/static/js/1.e0bc2cd7.chunk.js"
+  },
+  {
+    "revision": "cd37200121dd68d54219",
+    "url": "/static/css/main.ae31456c.chunk.css"
+  },
+  {
+    "revision": "dd4c935864d3b0a39649103ce9a0640d",
+    "url": "/index.html"
+  }
+];
